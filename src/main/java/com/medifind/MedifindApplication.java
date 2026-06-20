fada0ffe881f6dd88375e355e4cc8f0b2be15384
Arin Tholia <arin.thola@gmail.com>
2026-06-20 17:37:37 +0530

    Day 1 - Spring Boot setup and first API

---
+++ src/main/java/com/medifind/MedifindApplication.java
@@ -0,0 +1,13 @@
+package com.medifind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedifindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedifindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medifind/MedifindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medifind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
